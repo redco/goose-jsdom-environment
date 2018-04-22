36930e13e3b94f60d27083ad63c68d05601c657f
Andrew Red <andrew@reddikh.com>
2018-04-22 12:33:20 +0300

    added docker support
    :goose:

---
+++ docker/publish.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+environmentName="jsdom";
+environment="goose-${environmentName}-environment";
+environmentVersion=`npm show goose-${environmentName}-environment version`;
+
+echo "Building image for $1";
+IMAGE_NAME="redcode/$environment";
+TAG_NAME_VERSIONED="${environmentVersion}";
+TAG_NAME_LATEST="latest";
+docker build --build-arg ENVIRONMENT=$environment -t "$TAG_NAME_VERSIONED" -t "$TAG_NAME_LATEST" -f ./Dockerfile .
+DOCKER_NAME_VERSIONED="$IMAGE_NAME:$TAG_NAME_VERSIONED"
+DOCKER_NAME_LATEST="$IMAGE_NAME:$TAG_NAME_LATEST"
+docker tag "$TAG_NAME_VERSIONED" "$DOCKER_NAME_VERSIONED"
+docker tag "$TAG_NAME_LATEST" "$DOCKER_NAME_LATEST"
+docker push "$DOCKER_NAME_VERSIONED"
+docker push "$DOCKER_NAME_LATEST"
+docker rmi "$DOCKER_NAME_VERSIONED" "$DOCKER_NAME_LATEST"
